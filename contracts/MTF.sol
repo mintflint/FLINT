3a5a0d84655662d76a45a8859536749fd1399b90
Artem Pikulin <ortgma@gmail.com>
2018-05-07 19:08:28 +0700

    Fix typo, there was additional slash.

--- contracts/MTF.sol
+++ contracts/MTF.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.21;
 import './MintableToken.sol';
-import 'openzeppelin-solidity/contracts//ownership/Ownable.sol';
+import 'openzeppelin-solidity/contracts/ownership/Ownable.sol';
 
 contract MTF is MintableToken, Ownable {
 
